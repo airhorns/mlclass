53dae976f48a8ca12482260e938d36620a912da5
Harry Brundage <harry.brundage@gmail.com>
2011-11-06 21:01:07 -0500

    First part of assignment 2

--- mlclass-ex1/ex1_multi.m
+++ mlclass-ex1/ex1_multi.m
@@ -3,11 +3,11 @@
 %
 %  Instructions
 %  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear regression exercise. 
 %
-%  You will need to complete the following functions in this 
+%  This file contains code that helps you get started on the
+%  linear regression exercise.
+%
+%  You will need to complete the following functions in this
 %  exericse:
 %
 %     warmUpExercise.m
@@ -43,8 +43,8 @@ m = length(y);
 fprintf('First 10 examples from the dataset: \n');
 fprintf(' x = [%.0f %.0f], y = %.0f \n', [X(1:10,:) y(1:10,:)]');
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 % Scale features and set them to zero mean
 fprintf('Normalizing Features ...\n');
@@ -54,19 +54,18 @@ fprintf('Normalizing Features ...\n');
 % Add intercept term to X
 X = [ones(m, 1) X];
 
-
 %% ================ Part 2: Gradient Descent ================
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: We have provided you with the following starter
 %               code that runs gradient descent with a particular
-%               learning rate (alpha). 
+%               learning rate (alpha).
 %
-%               Your task is to first make sure that your functions - 
-%               computeCost and gradientDescent already work with 
+%               Your task is to first make sure that your functions -
+%               computeCost and gradientDescent already work with
 %               this starter code and support multiple variables.
 %
-%               After that, try running gradient descent with 
+%               After that, try running gradient descent with
 %               different values of alpha and see which one gives
 %               you the best result.
 %
@@ -85,7 +84,7 @@ fprintf('Running gradient descent ...\n');
 alpha = 0.01;
 num_iters = 100;
 
-% Init Theta and Run Gradient Descent 
+% Init Theta and Run Gradient Descent
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
@@ -104,7 +103,11 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+
+Xi = ([1650 3] .- mu) ./ sigma;
+Xi = [ones(1) Xi];
+
+price = Xi * theta; % You should change this
 
 
 % ============================================================
@@ -120,12 +123,12 @@ pause;
 fprintf('Solving with normal equations...\n');
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: The following code computes the closed form 
+% Instructions: The following code computes the closed form
 %               solution for linear regression using the normal
-%               equations. You should complete the code in 
+%               equations. You should complete the code in
 %               normalEqn.m
 %
-%               After doing so, you should complete this code 
+%               After doing so, you should complete this code
 %               to predict the price of a 1650 sq-ft, 3 br house.
 %
 
---
+++ mlclass-ex2/plotData.m
@@ -0,0 +1,28 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+%
+
+positiveIndexes = find(y == 1);
+negativeIndexes = find(y == 0);
+
+plot(X(positiveIndexes, 1), X(positiveIndexes, 2), 'r+', 'LineWidth', 2, 'markerSize', 10);
+plot(X(negativeIndexes, 1), X(negativeIndexes, 2), 'bo', 'LineWidth', 2, 'markerSize', 10);
+
+
+% =========================================================================
+
+
+
+hold off;
+
+end
